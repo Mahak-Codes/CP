977fa182515947ebb83be30fc4065fb56211162a
Mahak-Codes <mahakg2311@gmail.com>
2024-10-15 21:46:53 +0530

    optimised code

--- Codeforces/B_Binomial_Coefficients_Kind_Of.cpp
+++ Codeforces/B_Binomial_Coefficients_Kind_Of.cpp
@@ -4,7 +4,22 @@ using namespace std;
 #define int long long
 #define endl "\n"
 #define mahak_codes ios_base::sync_with_stdio(false); cin.tie(nullptr);
+int mod=1e9+7;
 
+int powm(int x,int n){
+    x%=mod;
+    if(n==0){
+        return 1;
+    }
+    if(n==1){
+        return x % mod;
+    }
+    int p=powm(x*x,n/2)%mod;
+    if(n&1){
+        p=p*x%mod;
+    }
+    return p%mod;
+}
 int32_t main() {
     mahak_codes
     int n;
@@ -18,10 +33,7 @@ int32_t main() {
         cin>>b[i];
     }
     for(int i=0;i<n;i++){
-        int ans=1;
-        for(int j=0;j<b[i];j++){
-            ans=(ans*2)%mod;
-        }
+        int ans=powm(2,b[i]);
         cout<<ans<<endl;
     }
     return 0;
